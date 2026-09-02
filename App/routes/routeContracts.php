75e440df61bf80501d2236a797631b65388b11bf
pcsouzareis <pcsreis@hotmail.com>
2026-09-02 20:26:00 -0300

    refatoração: Remover quebras de linha desnecessárias nas rotas de contratos

--- App/routes/routeContracts.php
+++ App/routes/routeContracts.php
@@ -7,10 +7,7 @@ use App\Middleware\AuthMiddleware;
 use Slim\App;
 
 return static function (App $app): void {
-    $app->get('/contratos', [ContractAcceptanceController::class, 'index'])
-        ->add(AuthMiddleware::class);
-    $app->get('/contratos/dados', [ContractAcceptanceController::class, 'data'])
-        ->add(AuthMiddleware::class);
-    $app->get('/contratos/{code:[0-9]+}/pdf', [ContractAcceptanceController::class, 'pdf'])
-        ->add(AuthMiddleware::class);
+    $app->get('/contratos', [ContractAcceptanceController::class, 'index'])->add(AuthMiddleware::class);
+    $app->get('/contratos/dados', [ContractAcceptanceController::class, 'data'])->add(AuthMiddleware::class);
+    $app->get('/contratos/{code:[0-9]+}/pdf', [ContractAcceptanceController::class, 'pdf'])->add(AuthMiddleware::class);
 };
